545cb6939984465c77b5ceb1964e3fcb61058b10
joan.tolos <joan.tolos@thomsonreuters.com>
2016-04-05 17:58:44 +0200

    Producer/Consumer example working

---
+++ rabbit-mq-producer/src/main/java/com/joantolos/rabbit/mq/producer/Producer.java
@@ -0,0 +1,28 @@
+package com.joantolos.rabbit.mq.producer;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+import java.util.concurrent.TimeoutException;
+
+public class Producer {
+
+    private final static String QUEUE_NAME = "my_messages";
+
+    public static void main(String[] args) throws java.io.IOException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        String message = "Message on the queue from the Producer";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        System.out.println(" [x] Sent '" + message + "'");
+        channel.close();
+        connection.close();
+
+    }
+}
